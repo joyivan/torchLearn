b13e9eef691ac1baff6053d75e80a10dee09c0b6
lizirong <ivanbear@163.com>
2023-10-05 21:21:51 +0800

    start learing transformer

---
+++ learnTransformer.py
@@ -0,0 +1,11 @@
+import torch
+sentence=[
+    ['ich mochte einbier P','S i want a beer .', 'i want a beer . E'],
+    ['ich mochte ein cola P', 'S i want a coke .', 'i want a coke . E']]
+src_vocab = {'P': 0, 'ich': 1, 'mochte': 2, 'ein': 3, 'bier': 4, 'cola': 5}
+src_vocab_size = len(src_vocab)
+
+tgt_vocab = {'P': 0, 'i': 1, 'want': 2, 'a': 3, 'beer': 4, 'coke': 5, 'S': 6, 'E': 7, '.': 8}
+idx2word = {i: w for i, w in enumerate(tgt_vocab)}
+# print(idx2word)
+tgt_vocab_size = len(tgt_vocab)
