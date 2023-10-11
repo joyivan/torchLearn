d2c6baa367e5265402a732c0ed1c1924dc2007bb
lizirong <ivanbear@163.com>
2023-10-11 11:21:40 +0800

    practice class decorate

--- testClass2.py
+++ testClass2.py
@@ -43,8 +43,68 @@ class student():
         student.NoOfStu+=1
 
 a={}
-for i in range(13):
+for i in range(9):
     temp=student()
     a.update({i:temp})
 print(len(a))
 print(a.keys())
+class univer():
+    instance=None
+    def __new__(cls, *args, **kwargs):
+        if univer.instance is None:
+              univer.instance=super().__new__(cls)
+              return univer.instance
+        else:
+            return univer.instance
+    def __init__(self,a,b):
+        self.a=a
+        self.b=b
+    def driver(self):
+        print(self.a,'|',self.b)
+a=univer(1,2)
+b=univer(3,4)
+print('eeee')
+print(a,b)
+print(a.driver(),b.driver())
+
+'''
+class A:
+    def foo(self):
+        print("A foo")
+
+class B(A):
+    def foo(self):
+        print("B foo")
+
+class C(A):
+    def foo(self):
+        print("C foo")
+
+class D(B, C):
+    def foo(self):
+        super().foo()  # 调用父类 A 的 foo() 方法
+
+d = D()
+d.foo()
+print('-'*40)
+class A:
+    def foo(self):
+        print("A foo")
+
+class B(A):
+    def foo(self):
+        super().foo()
+        print("B foo")
+
+class C(A):
+    def foo(self):
+        super().foo()
+        print("C foo")
+
+class D(B, C):
+    def foo(self):
+        super().foo()
+        print("D foo")
+d=D()
+print(D.mro())
+'''
