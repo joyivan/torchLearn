4d002e9f65305533ea6aa356d85e4206f78c4c37
lizirong <ivanbear@163.com>
2023-10-11 10:30:53 +0800

    practice class decorate

--- testClass.py
+++ testClass.py
@@ -10,8 +10,34 @@ class ani:
         print(f'U are the {self.total} animal in the world')
         print()
 a={}
-for i in range(10):
+for i in range(30):
     temp=ani()
     a.update({i:temp})
-print(a[9])
 
+print(len(a))
+class Animal:
+    def __init__(self,aname):
+        self.name = aname
+    def enjoy(self):
+        print("动物在叫")
+
+class Cat(Animal):
+    def enjoy(self):
+        print(self.name,"喵喵喵")
+
+class Dog(Animal):
+    def enjoy(self):
+        print(self.name,"汪汪汪")
+
+class Person:
+    def __init__(self,id,name):
+        self.name = name
+        self.id = id
+    def drive(self,ani):
+        ani.enjoy()
+
+cat = Cat("Mikey")
+dog = Dog("Dahuang")
+person = Person("张三",9)
+person.drive(cat)
+person.drive(dog)
--- testClass2.py
+++ testClass2.py
@@ -1,3 +1,4 @@
+import sys
 #def fun(a):
 #    print('in fun')
 #    def fun1(b):
@@ -9,20 +10,41 @@
 #c=mm(2)
 #print(c)
 import functools
-def add1(time):
-   def add3(a):
-       print(time)
+#def add1(time):
+#   def add3(a):
+#       print(time)
+#
+#       #@functools.wraps(a)
+#       def add2(c,b):
+#           print('a name is',id(a))
+#           return a(c,b)
+#
+#       return add2
+#   return add3
+#@add1(time=10)
+#def add(a,b):
+#    return a+b
+#c=add(1,2)
+#print(add.__name__)
+#print(c)
+class student():
+    NoOfStu=0
+    def __new__(cls, *args, **kwargs):
+       try:
+          if cls.NoOfStu<10:
+              print(cls.NoOfStu)
+              return super().__new__(cls)
+          else:
+              print('limit 10')
+              raise ValueError("fail to create student")
+       except ValueError as e:
+          sys.exit()
+    def __init__(self):
+        student.NoOfStu+=1
 
-       #@functools.wraps(a)
-       def add2(c,b):
-           print(id(a))
-           return a(c,b)
-
-       return add2
-   return add3
-@add1(time=10)
-def add(a,b):
-    return a+b
-c=add(1,2)
-print(add.__name__)
-print(c)
+a={}
+for i in range(13):
+    temp=student()
+    a.update({i:temp})
+print(len(a))
+print(a.keys())
