ae0af5f0f300e2a92e054bb1458179a69a1ee260
joyivan <ivanbear@163.com>
2023-10-14 17:12:29 +0800

    practice

--- testClass1.py
+++ testClass1.py
@@ -1,16 +1,32 @@
-def fun1(ff):
-    def fun2(a):
-        a=a+2
-        return ff(a)
-    return fun2
+#def fun1(ff):
+#    def fun2(a):
+#        a=a+2
+#        return ff(a)
+#    return fun2
+#
+#
+#
+#@fun1
+#def main(num):
+#    a=1
+#    for i in range(1,num+1):
+#        a*=i
+#    return a
+#
+#print(main(10))
+from numpy.random import randint as nn
 
 
+a=nn(2)
+print(a)
+from datetime import datetime,timedelta,datetime
+a=datetime(2023,10,10)
+print(a)
+'''
+import{module,package,package.package,package.module}
+from {module} import {class,constant,function}
+from {package} import {subpackage}
+from {subpackage} import {subsubpackage}
+from {package.module} import {class,constant,function}
 
-@fun1
-def main(num):
-    a=1
-    for i in range(1,num+1):
-        a*=i
-    return a
-
-print(main(10))
+'''
